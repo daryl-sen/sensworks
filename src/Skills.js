a6169b0f9b9b9aef005bb26f66d29ed621ec4f0a
Daryl <tsdaryl@gmail.com>
2021-03-26 00:14:18 -0700

    add skill page

--- src/Skills.js
+++ src/Skills.js
@@ -1,6 +1,36 @@
+import './Skills.scss';
+
 function Skills(props) {
   return (
-    <p>Skills</p>
+    <>
+      <h3>Skills</h3>
+      <div class="three-columns">
+        <div>
+          <article>
+            <h4>Programming Languages</h4>
+            <p>
+              Javascript, Python, Ruby, PHP, HTML, CSS
+            </p>
+          </article>
+        </div>
+        <div>
+          <article>
+            <h4>Frameworks and Libraries</h4>
+            <p>
+              ReactJS, Django, Flask, Express, Laravel, NodeJS, Ajax, JQuery, mySQL, PostgreSQL, SQLite
+            </p>
+          </article>
+        </div>
+        <div>
+          <article>
+            <h4>Server Technologies</h4>
+            <p>
+              Nginx, uWSGI, Heroku CLI, PM2, Certbot
+            </p>
+          </article>
+        </div>
+      </div>
+    </>
 
   );
 }
--- src/Summary.js
+++ src/Summary.js
@@ -2,9 +2,9 @@ function Summary(props) {
   return (
     <section>
       <h3>Summary</h3>
-      <p>
+      <article>
         A web developer who enjoys using web technologies to solve everyday problems. I created web apps currently used by a research lab at Simon Fraser University, reducing workload and decreasing production time for experiments by almost 200%. I worked with local small businesses, friends, and family to create public-facing websites and systems.
-      </p>
+      </article>
     </section>
   );
 }
