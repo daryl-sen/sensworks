706133ba48e4dbffc0732791ff40121b1a9209e4
daryl <tsdaryl@gmail.com>
2021-04-26 16:40:19 -0700

    remove all old files

--- README.md
+++
@@ -1,3 +0,0 @@
-# www.sensworks.ca
-
-My personal online resume. More work to be done to improve the user experience, currently MVP-only.
--- src/components/TechDetails.js
+++
@@ -1,9 +0,0 @@
-import './TechDetails.scss';
-
-function TechDetails() {
-  return (
-    <h1>TechDetails</h1>
-  );
-}
-
-export default TechDetails;
